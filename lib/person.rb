bc58cf5d0c58126bc7b411aee45ca10b83d0609d
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-12 17:22:36 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,11 +3,11 @@ class Person
     @name = name
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
+  # def name=(name)
+  #   @name = name
+  # end
+  # 
+  # def name
+  #   @name
+  # end
 end
