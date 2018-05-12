b293badd1acd2065754ca2cd8aed381dc4e4e2cb
Theresa Morelli <theresa.l.morelli@gmail.com>
2018-05-12 17:22:18 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,12 +2,12 @@ class Person
   def initialize(name)
     @name = name
   end
- 
-  def breed=(breed)
-    @breed = breed
+
+  def name=(name)
+    @name = name
   end
- 
-  def breed
-    @breed
+
+  def name
+    @name
   end
 end
